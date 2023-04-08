16697e0a017ac80dcb27059177ce3860f18f226b
William Wang <wmwang52@gmail.com>
2023-04-08 12:51:58 -0400

    Created Baseline Xcode iOS Project File

---
+++ Breathe Easy/Breathe Easy/Breathe_EasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breathe_EasyApp.swift
+//  Breathe Easy
+//
+//  Created by William Wang on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Breathe_EasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
